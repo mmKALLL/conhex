41fbfe7b34703b5fdb4616c6eca50f227f8ffe46
Esa Koskinen <esakoskin2@gmail.com>
2021-09-05 23:38:06 +0900

    prettier formatting

--- src/utils/gamestate-utils.ts
+++ src/utils/gamestate-utils.ts
@@ -1,4 +1,4 @@
-import { Node } from "./board-utils"
+import { Node } from './board-utils'
 
 type Player = {
   name: string
@@ -41,4 +41,3 @@ const readGame = (sgfText: string): GameState => {
 readGame(
   `(;FF[CONHEX]VA[CONHEX]EV[conhex.ch.21.1.1]PB[David Milne]PW[leandro ?]SO[https://www.littlegolem.net];B[J3];R[H5];B[I6];R[I8];B[H7];R[I4];B[J4];R[I7];B[H6];R[J6];B[I5];R[G9];B[E8];R[F8];B[C10];R[D9];B[C8];R[B8];B[B9];R[D6];B[D7];R[F7];B[G8];R[F9];B[C5];R[C6];B[D5];R[E3];B[C2];R[C4];B[D3];R[resign])`
 )
-
